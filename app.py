ed57cd03330c4f5acfc99c5c5c5eb33b5c858876
Pinquei <pierce123455@gmail.com>
2023-09-22 18:01:28 +0800

    improve something

--- app.py
+++ app.py
@@ -52,66 +52,67 @@ def update_data(raspberry_id):
 
 @app.route('/data/<int:raspberry_id>')
 def get_data(raspberry_id):
-    print("raspberry_data",raspberry_data)
-    conn = sqlite3.connect('raspberry_data.db')
-    cursor = conn.cursor()
+    #print("raspberry_data",raspberry_data)
+    if raspberry_id==1:
+        conn = sqlite3.connect('raspberry_data.db')
+        cursor = conn.cursor()
 
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    now_time=time.time()
-    for location, mac_data in raspberry_data.items():
-        for mac, strength in mac_data.items():
-            # 檢查資料庫是否已經有這個mac資料
-            cursor = conn.execute("SELECT * FROM mac_tracking WHERE mac = ? ORDER BY rowid DESC LIMIT 1", (mac,))
-            existing_data = cursor.fetchone()
-            print('existing_data_test',existing_data)
-            if existing_data:
-                print(existing_data)
-                # 如果mac的location有變化，更新disappearance_time
-                if existing_data[2] != location:
-                    conn.execute(
-                        "UPDATE mac_tracking SET disappearance_time = ? WHERE mac = ? AND disappearance_time IS NULL",
-                        (current_time, mac))
-                    #print('update')
-                    conn.commit()
+        current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        now_time=time.time()
+        for location, mac_data in raspberry_data.items():
+            for mac, strength in mac_data.items():
+                # 檢查資料庫是否已經有這個mac資料
+                cursor = conn.execute("SELECT * FROM mac_tracking WHERE mac = ? ORDER BY rowid DESC LIMIT 1", (mac,))
+                existing_data = cursor.fetchone()
+                print('existing_data_test',existing_data)
+                if existing_data:
+                    print(existing_data)
+                    # 如果mac的location有變化，更新disappearance_time
+                    if existing_data[2] != location:
+                        conn.execute(
+                            "UPDATE mac_tracking SET disappearance_time = ? WHERE mac = ? AND disappearance_time IS NULL",
+                            (current_time, mac))
+                        #print('update')
+                        conn.commit()
+                        insert_data = (mac, location, current_time, None)
+                        conn.execute(
+                            "INSERT INTO mac_tracking (mac, location, appearance_time, disappearance_time) VALUES (?, ?, ?, ?)",
+                            insert_data)
+                        #print('insert')
+                        last_time=time.time()
+                        conn.commit()
+                    elif existing_data[4] != None:
+                        insert_data = (mac, location, current_time, None)
+                        conn.execute(
+                            "INSERT INTO mac_tracking (mac, location, appearance_time, disappearance_time) VALUES (?, ?, ?, ?)",
+                            insert_data)
+                        print('insert')
+                            
+                else:
+                    # 新增一筆新的資料
                     insert_data = (mac, location, current_time, None)
                     conn.execute(
                         "INSERT INTO mac_tracking (mac, location, appearance_time, disappearance_time) VALUES (?, ?, ?, ?)",
                         insert_data)
-                    #print('insert')
+                    #print('insertblblblba')
                     last_time=time.time()
                     conn.commit()
-                elif existing_data[4] != None:
-                    insert_data = (mac, location, current_time, None)
-                    conn.execute(
-                        "INSERT INTO mac_tracking (mac, location, appearance_time, disappearance_time) VALUES (?, ?, ?, ?)",
-                        insert_data)
-                    print('insert')
-                        
-            else:
-                # 新增一筆新的資料
-                insert_data = (mac, location, current_time, None)
-                conn.execute(
-                    "INSERT INTO mac_tracking (mac, location, appearance_time, disappearance_time) VALUES (?, ?, ?, ?)",
-                    insert_data)
-                #print('insertblblblba')
-                last_time=time.time()
+
+        # 檢查是否有mac在新的一筆dict中消失了
+        cursor = conn.execute("SELECT mac FROM mac_tracking WHERE disappearance_time IS NULL")
+        existing_macs = set(row[0] for row in cursor.fetchall())
+
+        for mac in existing_macs:
+            if mac not in {mac for mac_data in raspberry_data.values() for mac in mac_data.keys()}:
+                # mac在新的一筆dict中消失，補充disappearance_time
+                conn.execute("UPDATE mac_tracking SET disappearance_time = ? WHERE mac = ? AND disappearance_time IS NULL",
+                            (current_time, mac))
+                print('update')
                 conn.commit()
 
-    # 檢查是否有mac在新的一筆dict中消失了
-    cursor = conn.execute("SELECT mac FROM mac_tracking WHERE disappearance_time IS NULL")
-    existing_macs = set(row[0] for row in cursor.fetchall())
-
-    for mac in existing_macs:
-        if mac not in {mac for mac_data in raspberry_data.values() for mac in mac_data.keys()}:
-            # mac在新的一筆dict中消失，補充disappearance_time
-            conn.execute("UPDATE mac_tracking SET disappearance_time = ? WHERE mac = ? AND disappearance_time IS NULL",
-                        (current_time, mac))
-            print('update')
-            conn.commit()
-
-        # Commit the changes and close the database connection
-    conn.commit()
-    conn.close()
+            # Commit the changes and close the database connection
+        conn.commit()
+        conn.close()
 
     return jsonify(raspberry_data.get(raspberry_id, {}))
     
